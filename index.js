2784bab84307182fdfdae66f45078b5876b441d4
vighnesh192 <vighnesh192000@gmail.com>
2022-02-03 12:10:35 +0530

    INIT

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
+app.listen(port, () => {
+    console.log('Server running on port 8080');
+})
