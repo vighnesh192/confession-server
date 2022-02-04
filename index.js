7f1c40ff83507486bfc312c8bf3c1b4f5d74e986
vighnesh192 <vighnesh192000@gmail.com>
2022-02-04 18:47:44 +0530

    Fix: bodyParser and cors

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser')
 const userRouter = require('./routes/user');
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(cors());
 
 const port = process.env.PORT || 8080;
 
