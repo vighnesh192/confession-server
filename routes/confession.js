7b2d17c5ac5f1054dc21672a095285084be788fb
vighnesh192 <vighnesh192000@gmail.com>
2022-02-14 22:37:35 +0530

    Feat: Created /confessions/:college route

--- routes/confession.js
+++ routes/confession.js
@@ -1,15 +1,22 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
 const { ensureAuth } = require('../middlewares/auth');
 const Confession = require('../models/Confession');
 require('dotenv').config();
-const GoogleUser = require('../models/Google_User');
-const User = require('../models/User');
 const router = express.Router();
 
 router.get('/confessions', async (req, res) => {
     try {
         const confessions = await Confession.find().exec();
+        res.json({ confessions, success: true });
+    } catch (error) {
+        console.log("Error:-", error);
+        res.json({ error, success: false });
+    }
+})
+
+router.get('/confessions/:college', async (req, res) => {
+    try {
+        const confessions = await Confession.find({ college: req.params.college }).exec();
         res.json({ confessions, success: true });
     } catch (error) {
         console.log("Error:-", error);
