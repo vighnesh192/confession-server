992f282aabdaefc688d5bffb34bfdfae000a0b7f
vighnesh192 <vighnesh192000@gmail.com>
2022-04-19 15:50:57 +0530

    PROD: meta tag added for Google Search Console verification

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser')
-const userRouter = require('./routes/user');
-const confessionRouter = require('./routes/confession');
-const collegeRouter = require('./routes/college');
-const sortRouter = require('./routes/sort');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const userRouter = require("./routes/user");
+const path = require("path");
+
+const confessionRouter = require("./routes/confession");
+const collegeRouter = require("./routes/college");
+const sortRouter = require("./routes/sort");
 
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
@@ -20,25 +22,28 @@ app.use(cors());
 
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-})
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
-app.use('/user', userRouter);
-app.use('/confession', confessionRouter);
-app.use('/college', collegeRouter);
-app.use('/sort', sortRouter);
+app.use("/user", userRouter);
+app.use("/confession", confessionRouter);
+app.use("/college", collegeRouter);
+app.use("/sort", sortRouter);
 
 //DB Config
 const url = process.env.MONGO_URI;
 //Connect To DB
-mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => {console.log("DB CONNECTED...")})
-.catch((err) => console.log(err));
+mongoose
+    .connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("DB CONNECTED...");
+    })
+    .catch((err) => console.log(err));
 
 app.listen(port, () => {
-    console.log('Server running on port 8080');
-})
+    console.log("Server running on port 8080");
+});
