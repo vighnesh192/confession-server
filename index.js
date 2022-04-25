ff8c5b7ed4a9e7095aaeb2ce26631763160039c1
vighnesh192 <vighnesh192000@gmail.com>
2022-04-25 16:58:12 +0530

    OAuth Verification:- Added Homepage, Privacy Policy and Favicon

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRouter = require("./routes/user");
 const path = require("path");
+const favicon = require('serve-favicon');
 
 const confessionRouter = require("./routes/confession");
 const collegeRouter = require("./routes/college");
 const sortRouter = require("./routes/sort");
 
 const app = express();
+
+// app.use(favicon(path.join(__dirname, 'public', 'assets', 'favicon.ico')));
+app.use(express.static('public'));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +28,10 @@ const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/privacy-policy", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/PrivacyPolicy.html"));
 });
 
 app.use("/user", userRouter);
